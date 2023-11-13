c6f8eac7e4617dd4c763de7a012ed90df028f4d2
Shukurulla <shukurullatursinbayev978@gmail.com>
2023-11-13 13:26:38 +0500

    Edit Cors

--- routers/food.js
+++ routers/food.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Food = require("../models/foods");
 const router = express.Router();
 const cors = require("cors");
+const FileReader = require("filereader");
 
 router.get("/foods", cors(), async (req, res) => {
   const foods = await Food.find();
@@ -9,7 +10,15 @@ router.get("/foods", cors(), async (req, res) => {
 });
 
 router.post("/foods-create", cors(), async (req, res) => {
-  await Food.create(req.body);
+  let link = "";
+  const reader = new FileReader();
+
+  reader.addEventListener("load", async () => {
+    link = reader.result;
+  });
+
+  reader.readAsDataURL(req.body.image);
+  await Food.create({ ...req.body, image: link });
   const foods = await Food.find();
   res.json({ data: foods });
 });
