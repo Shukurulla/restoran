1a8afd476812f30889baf4fa10e857e90023ab17
Shukurulla <shukurullatursinbayev978@gmail.com>
2023-11-13 12:04:01 +0500

    Edit Cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(FoodRouter);
 app.use(require("./routers/category"));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     optionsSuccessStatus: 200,
     credentials: true,
   })
--- routers/category.js
+++ routers/category.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const Category = require("../models/category");
 const router = express.Router();
+const cors = require("cors");
 
-router.get("/categories", async (req, res) => {
+router.get("/categories", cors(), async (req, res) => {
   const categories = await Category.find();
   res.json({ data: categories });
 });
 
-router.post("/categories", async (req, res) => {
+router.post("/categories", cors(), async (req, res) => {
   await Category.create(req.body);
   const categories = await Category.find();
   res.json({ data: categories });
