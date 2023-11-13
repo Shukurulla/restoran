37724852fe9bc7dde5a1558a9a951c57962ecf45
Shukurulla <shukurullatursinbayev978@gmail.com>
2023-11-13 12:02:03 +0500

    Edit Cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(FoodRouter);
 app.use(require("./routers/category"));
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     optionsSuccessStatus: 200,
     credentials: true,
   })
