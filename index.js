657c7c1cec41ce89bc319571c3297b6d07174b4d
Shukurulla <shukurullatursinbayev978@gmail.com>
2023-11-13 12:33:00 +0500

    Edit Cors

--- index.js
+++ index.js
@@ -10,14 +10,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(FoodRouter);
 app.use(require("./routers/category"));
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  })
-);
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.options("/foods-create", cors());
 app.options("/categories", cors());
 
