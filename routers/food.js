4dcb6310faeeb650572e844d8edfc37f5e3326c5
Shukurulla <shukurullatursinbayev978@gmail.com>
2023-12-03 10:33:35 +0500

    edit project

--- routers/food.js
+++ routers/food.js
@@ -7,7 +7,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "/public/Images"));
+    cb(null, "/public/Images");
   },
   filename: (req, file, cb) => {
     cb(
