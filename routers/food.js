2c7b620cfb6ce975b1a0185ac03d18bc3d93a523
Shukurulla <shukurullatursinbayev978@gmail.com>
2023-12-04 12:29:14 +0500

    project edit

--- routers/food.js
+++ routers/food.js
@@ -2,8 +2,6 @@ const express = require("express");
 const Food = require("../models/foods");
 const router = express.Router();
 const cors = require("cors");
-const multer = require("multer");
-const path = require("path");
 
 router.get("/foods", cors(), async (req, res) => {
   const foods = await Food.find();
@@ -20,7 +18,6 @@ router.post("/edit-food/:id", cors(), async (req, res) => {
   await Food.findByIdAndUpdate(id, req.body);
   const data = await Food.find();
   res.json({ data: data });
-  console.log(req.files);
 });
 router.post("/delete-food/:id", cors(), async (req, res) => {
   const id = req.params.id;
